25ffd6ebc630a4144d661e1fa5b7fdd9ed0996c0
alejandrocastillosk8-ctrl <alejandrocastillosk8@gmail.com>
2026-04-18 16:29:34 -0500

    first commit

---
+++ js/New folder/new.js
@@ -0,0 +1,23 @@
+let nombre = 1
+
+for ( let nombre = 1; nombre <=10; nombre++ ) {
+    if (nombre % 2 === 0) {
+    console.log(nombre)
+    }
+    
+}
+let suma = 0
+for ( let nombre = 1 ; nombre <=10;nombre++ ) { suma = suma + nombre}
+    console.log(suma)
+
+
+
+
+
+
+
+
+let s = 0
+let a = 1
+for ( let a = 1 ; a <=20 ; a ++) 
+    if ( a %2 !== 0)    console.log (a)   
